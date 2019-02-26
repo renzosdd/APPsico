f4d29caab0d0162d07eaacb66d234749da9d04df
Renzo Dogliotti <44270754+renzosdd@users.noreply.github.com>
2019-02-26 17:57:47 -0300

    Avanzando metodos ABM pacientes

--- app/APPsico.py
+++ app/APPsico.py
@@ -30,6 +30,13 @@ class Paciente:
         except:
             pass
 
+    def baja(self,id_paciente):
+        self.id=id_paciente
+        try:
+            query('DELETE FROM pacientes WHERE id_paciente = '+self.id+';')
+        except:
+            pass
+
     #Metodo para consultar pacientes pasandole la sentencia SQL
     def consulta(self,sql):
         row=query(sql).fetchall()
@@ -46,64 +53,47 @@ class interfazPacientes:
         self.entryBuscarPaciente=Entry(self.pestanaPaciente)
         self.entryBuscarPaciente.grid(row=0, column=0, pady=5,padx=1)
         self.btnBuscarPaciente=ttk.Button(self.pestanaPaciente,text="Buscar")
-        self.btnBuscarPaciente.grid(row=0,column=2, pady=5,padx=1) 
+        self.btnBuscarPaciente.grid(row=0,column=3, pady=5,padx=1) 
+        self.btnRefrescarPacientes=ttk.Button(self.pestanaPaciente,text="Refrescar lista",command=self.refresco)
+        self.btnRefrescarPacientes.grid(row=1,column=3, pady=5,padx=1) 
         self.listboxPaciente=Listbox(self.pestanaPaciente)
         self.listboxPaciente.grid(row=1,column=0,columnspan=2)
         self.scllVPaciente=ttk.Scrollbar(self.pestanaPaciente,command=self.listboxPaciente.yview)
-        self.scllVPaciente.grid(row=1,column=3, sticky="nsew")
+        self.scllVPaciente.grid(row=1,column=2, sticky="nsew")
         self.listboxPaciente.config(yscrollcommand=self.scllVPaciente.set)
-        return self.pestanaPaciente
-
-class Aplicacion:
-    # Declaramos una variable para controlar cantidad de de ventanas
-    ventana = 0
-
-    def __init__(self):
-        #self.pacientes = Paciente()
-        #self.listapacientes = self.pacientes.consulta("SELECT * FROM pacientes")
-        #Definicion de ventana del programa
-        self.ventanaPrincipal=Tk()
-        #self.ventanaPrincipal.geometry('500x500+200+50')
-        self.ventanaPrincipal.title("APPSico - El bosque")
-        self.ventanaPrincipal.resizable(0,0)
-        #Definicion Gestor de pestañas
-        self.pestanasPrincipal = ttk.Notebook(self.ventanaPrincipal)
-        self.pestanasPrincipal.pack(expand=True, fill=BOTH)  
-        #Definicion pestaña pacientes
-        self.pestanaPaciente=interfazPacientes(self.pestanasPrincipal)
-        self.pestanasPrincipal.add(self.pestanaPaciente.crearInterfazPaciente(), text="Pacientes")
-        #Definicion pestaña sesiones
-
-        #Prueba para abrir nueva ventana
-        boton = ttk.Button(self.ventanaPrincipal, text='Abrir', command=self.abrir)
-        boton.pack(side=BOTTOM, padx=20, pady=20)
         self.refresco()
-        
+        #Prueba para abrir nueva ventana
+        self.btnNuevoPaciente = ttk.Button(self.pestanaPaciente, text='Nuevo Paciente', command=self.nuevoPaciente)
+        self.btnNuevoPaciente.grid(row=2,column=3)
+        return self.pestanaPaciente
     
-        self.ventanaPrincipal.mainloop()
-    
-    def abrir(self):
-        ''' Construye una ventana de diálogo '''
+    def refresco(self):
+        self.pacientes = Paciente()
+        self.listapacientes = self.pacientes.consulta("SELECT * FROM pacientes")
+        #limpiamoslistview
+        self.listboxPaciente.delete(0, END)
+        #Refrescamos la lista de pacientes
+        count = 0
+        for paciente in self.listapacientes:
+            self.listboxPaciente.insert(count,paciente[1])
+            count+=1
+
+    def nuevoPaciente(self):
         if (Aplicacion.ventana == 0):
-            # Define una nueva ventana de diálogo
-            
+
+            #Creamos una ventana de dialogo
             self.dialogo = Toplevel()
             
-            # Incrementa en 1 el contador de ventanas
-            
+            # Incrementa en 1 el contador de ventanas para controlar que solo se abra 1
             Aplicacion.ventana+=1
             
-
             #self.dialogo.geometry('500x500+200+50')
             self.dialogo.resizable(0,0)
             
-            # Obtiene identicador de la nueva ventana 
-            
-            ident = self.dialogo.winfo_id()
-            
-            
-            titulo = str(Aplicacion.ventana)+": "+str(ident)
-            self.dialogo.title(titulo)
+            # Obtiene identicador de la nueva ventana       
+            #ident = self.dialogo.winfo_id()
+            #titulo = str(Aplicacion.ventana)+": "+str(ident)
+            self.dialogo.title("Nuevo Paciente")
 
             nombreLabel=Label(self.dialogo, text="Nombre: ")
             nombreLabel.grid(row=0, column=0, sticky="e", pady=5, padx=1)
@@ -138,7 +128,7 @@ class Aplicacion:
             scrollVert.grid(row=4,column=2, sticky="nsew")
             comentariosText.config(yscrollcommand=scrollVert.set)
 
-            def codigoBoton():
+            def altaPaciente():
                 nombre = nombretxt.get()
                 apellido = apellidotxt.get()
                 mail=mailtxt.get()
@@ -147,34 +137,41 @@ class Aplicacion:
                 try:
                     self.pacientes.alta(nombre,apellido,mail,telefono,notas)
                     messagebox.showinfo("Se inserto correctamente", "El paciente" +nombre+" "+apellido+" fue creado correctamente")
-                    nombre.txt.set("")
+                    nombretxt.set("")
                     apellidotxt.set("")
                     mailtxt.set("")
                     teltxt.set("")
                 except:
                     print("Error no se pudo crear")
 
-
-            botonEnvio=ttk.Button(self.dialogo,text="enviar",command=codigoBoton)
+            botonEnvio=ttk.Button(self.dialogo,text="enviar",command=altaPaciente)              
             botonEnvio.grid(row=5,column=0,columnspan=2)
             boton = ttk.Button(self.dialogo, text='Cerrar',command=self.dialogo.destroy)  
             boton.grid(row=6,column=0,columnspan=2) 
-
-            self.ventanaPrincipal.wait_window(self.dialogo)
+            #self.ventanaPrincipal.wait_window(self.dialogo)
             Aplicacion.ventana=0
 
-    def refresco(self):
-        self.pacientes = Paciente()
-        self.listapacientes = self.pacientes.consulta("SELECT * FROM pacientes")
+class Aplicacion:
+    # Declaramos una variable para controlar cantidad de de ventanas
+    ventana = 0
 
-        #if listviewtienealgo:
-        #limpiamoslistview
-        #else:
-        #Refrescamos la lista de pacientes
-        count = 0
-        for paciente in self.listapacientes:
-            self.pestanaPaciente.listboxPaciente.insert(count,paciente[1])
-            count+=1
-        count=0
+    def __init__(self):
+        self.pacientes = Paciente()
+        #self.listapacientes = self.pacientes.consulta("SELECT * FROM pacientes")
+        #Definicion de ventana del programa
+        self.ventanaPrincipal=Tk()
+        #self.ventanaPrincipal.geometry('500x500+200+50')
+        self.ventanaPrincipal.title("APPSico - El bosque")
+        self.ventanaPrincipal.resizable(0,0)
+        #Definicion Gestor de pestañas
+        self.pestanasPrincipal = ttk.Notebook(self.ventanaPrincipal)
+        self.pestanasPrincipal.pack(expand=True, fill=BOTH)  
+        #Definicion pestaña pacientes
+        self.pestanaPaciente=interfazPacientes(self.pestanasPrincipal)
+        self.pestanasPrincipal.add(self.pestanaPaciente.crearInterfazPaciente(), text="Pacientes")
+        #Definicion pestaña sesiones
+        
+    
+        self.ventanaPrincipal.mainloop()
 
 APPSico=Aplicacion()

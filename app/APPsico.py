cb752347de6279a1d81ce1a91c79840eb142167b
renzosdd <44270754+renzosdd@users.noreply.github.com>
2019-03-05 20:47:10 -0300

    arreglo minimo .get de ifazPrincipal

--- app/APPsico.py
+++ app/APPsico.py
@@ -9,7 +9,7 @@ class Aplicacion:
         self.ventanaPrincipal = Tk()
         self.ventanaPrincipal.title("APPSico - El Bosque")
         self.ventanaPrincipal.resizable(0, 0)
-        F.IfazPrincipal(self.ventanaPrincipal).get
+        F.IfazPrincipal(self.ventanaPrincipal).get()
         self.ventanaPrincipal.mainloop()
 
 APPSico = Aplicacion()

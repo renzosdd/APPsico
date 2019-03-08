f75dd6435b40fdc61b3f7f39c7483eb09ddbd57d
Carlos Bordolli <carlosbordolli@gmail.com>
2019-03-07 23:11:37 -0300

    Update BackSesiones.py

--- app/BackSesiones.py
+++ app/BackSesiones.py
@@ -5,6 +5,14 @@ class Sesion:
     def __init__(self):
         pass
 
+    def alta(self, notas, inicio, fin, id_paciente):
+        try:
+            B.query('INSERT INTO sesiones (notas, inicio, fin, id_paciente) VALUES ("'+notas+'","'+inicio+'","'+fin+'","'+id_paciente+'");')
+        except:
+            pass
+
+
+
     def consulta(self, sql):
         row = B.query(sql).fetchall()
         return row

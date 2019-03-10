2f9d649bf6e53474f28b4bd87d6516bd660aeb41
renzosdd <44270754+renzosdd@users.noreply.github.com>
2019-03-09 22:46:52 -0300

    Se une el Login al FrontEnd
    Se unifica para que quede separado en 2 modulos, FrontEnd y BackEnd

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,14 +1,10 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from tkinter import scrolledtext as st
 from tkinter.ttk import *
 import sys
-import BackEnd as B
-from tkinter import messagebox
-import ClickDerecho as D
 from sys import platform as _platform
-
-
+import BackEnd as B
 
 class IfazPrincipal:
     def __init__(self, ifazLogin,usuario):
@@ -324,3 +320,69 @@ def menuClickDerecho(r):
         print (' - menuClickDerecho, something wrong')
         pass
 
+
+class Aplicacion():
+    def __init__(self):
+        self.raiz = Tk()
+        self.raiz.title("APPSico - El Bosque")
+        logo = PhotoImage(file="APPsico.png")
+        Label(self.raiz, compound = CENTER, text="", image=logo).pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=5)
+        self.raiz.iconbitmap('APPsico.ico')
+        self.lblUsuario = ttk.Label(self.raiz, text="Usuario:")
+        self.lblClave = ttk.Label(self.raiz, text="Contraseña:")
+        self.usuario = StringVar()
+        self.clave = StringVar()
+        
+        self.txtUsuario = ttk.Entry(self.raiz, 
+                                textvariable=self.usuario, 
+                                width=30)
+        self.txtClave = ttk.Entry(self.raiz, 
+                                textvariable=self.clave, 
+                                width=30, show="*")
+        self.separador = ttk.Separator(self.raiz, orient=HORIZONTAL)
+        
+        self.btnAceptar = ttk.Button(self.raiz, text="Aceptar", 
+                                 command=self.aceptar)
+        self.btnCancelar = ttk.Button(self.raiz, text="Cancelar", 
+                                 command=quit)
+                                         
+        self.lblUsuario.pack(side=TOP, fill=BOTH, expand=True, 
+                        padx=5, pady=5)
+        self.txtUsuario.pack(side=TOP, fill=X, expand=True, 
+                         padx=5, pady=5)
+        self.lblClave.pack(side=TOP, fill=BOTH, expand=True, 
+                        padx=5, pady=5)
+        self.txtClave.pack(side=TOP, fill=X, expand=True, 
+                         padx=5, pady=5)
+        self.separador.pack(side=TOP, fill=BOTH, expand=True, 
+                         padx=5, pady=5)
+        self.btnAceptar.pack(side=LEFT, fill=BOTH, expand=True, 
+                         padx=5, pady=5)
+        self.btnCancelar.pack(side=RIGHT, fill=BOTH, expand=True, 
+                         padx=5, pady=5)
+        
+        self.txtUsuario.focus_set()
+        self.raiz.mainloop()
+    
+    def aceptar(self):
+        try:
+            listaUsuarios=B.query("SELECT * FROM usuarios WHERE usuario='"+self.usuario.get().lower()+"'").fetchall()
+            if(listaUsuarios[0][1] == self.clave.get()):
+                self.raiz.withdraw()
+                IfazPrincipal(self.raiz,self.usuario.get().lower())
+                self.clave.set("")
+            else:
+                messagebox.showinfo("Error", "Usuario o clave invalida - Intentelo nuevamente")
+                self.clave.set("")
+                self.txtClave.focus_set()
+        except:
+            messagebox.showinfo("Error", "Usuario o clave invalida - Intentelo nuevamente")
+            self.clave.set("")
+            self.txtClave.focus_set()
+        
+def main():
+    Aplicacion()
+    return 0
+
+if __name__ == '__main__':
+    main()

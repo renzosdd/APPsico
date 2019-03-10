8453ee08794f7c22977d715be15987249e52b6bf
renzosdd <44270754+renzosdd@users.noreply.github.com>
2019-03-09 23:51:10 -0300

    BackEnd Sesiones
    Se programa ABM de Sesiones y se prueba en el archivo BackEnd (Funciona OK)

--- BackEnd.py
+++ BackEnd.py
@@ -49,6 +49,25 @@ class Sesion:
         except:
             pass
 
+    def modificar(self, id_sesion, notas, inicio, fin):
+        try:
+            query("UPDATE sesiones SET notas='"+notas+"',inicio='"+inicio+"',fin='"+fin+"' WHERE id_sesion="+id_sesion+";")
+        except:
+            pass
+
+    def baja(self, id_sesion):
+        try:
+            query('DELETE FROM sesiones WHERE id_sesion = '+id_sesion+';')
+        except:
+            pass
+
     def consulta(self, sql):
         row = query(sql).fetchall()
         return row
+
+#sesion=Sesion()
+#sesion.modificar("4","Nota Modificada","20190221 2:30:00 PM","20190224 3:55:00 PM")
+#sesion.baja("10")
+#sesion.alta("Es una nota nueva","20190221 2:30:00 PM","20190224 3:55:00 PM","63")
+#for sesion in sesion.consulta("SELECT * FROM sesiones"):
+#    print(sesion)
